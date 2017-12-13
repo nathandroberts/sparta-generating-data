53fa153c884c9599003e618e8f374bcbafd1504c
Tech-A35 <Tech-A35@Tech-A35s-MacBook-Air.local>
2017-12-13 15:23:08 +0000

    YAML and Faker data created and used

---
+++ faker/faker_intro.rb
@@ -0,0 +1,26 @@
+require 'faker'
+
+puts Faker::GameOfThrones.character
+puts 'was a'
+puts Faker::ElderScrolls.race
+puts 'from'
+puts Faker::LordOfTheRings.location
+puts '.Upon waking up everyday he would recite his sacred chant'
+puts Faker::HarryPotter.quote
+puts 'in the hope that it would give to confidence to finally approach his one true love'
+puts Faker::FamilyGuy.character
+puts 'One day during his job as a'
+puts Faker::Job.title
+puts 'he met'
+puts Faker::DragonBall.character
+puts 'who said'
+puts Faker::TheFreshPrinceOfBelAir.quote
+puts 'These wise words inspired him to become a superhero called'
+puts Faker::Superhero.prefix
+puts Faker::Superhero.name
+puts 'and develop the power of'
+puts Faker::Superhero.power
+puts 'where he fought crime with his pet'
+puts Faker::Dog.meme_phrase
+puts Faker::Pokemon.name
+puts 'and he lived happily ever after.'
